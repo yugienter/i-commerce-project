0360db2e04831d9eaf9e459e055e1278718b5209
yugienter <phu.nguyenluu@gmail.com>
2021-06-26 15:11:26 +0700

    iCommerce create

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [ProductModule],
+  controllers: [AppController, ProductController],
+  providers: [AppService, ProductService],
+})
+export class AppModule {}
